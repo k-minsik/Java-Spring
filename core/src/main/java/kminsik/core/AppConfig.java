0436e3cfced077322898f9be322108f3029b3875
k-minsik <smin58810@gmail.com>
2023-11-06 11:00:27 +0900

    feat: Test singleton pattern

--- core/src/main/java/kminsik/core/AppConfig.java
+++ core/src/main/java/kminsik/core/AppConfig.java
@@ -17,16 +17,24 @@ public class AppConfig {
 
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
+//        if (memoryMemberRepository가 이미 스프링 컨테이너에 등록) {
+//          return 스프링 컨테이너에서 찾아서 반환;
+//        } else { 스프링 컨테이너에 없으면
+//            기존 로직을 호출해서 MemoryMemberRepository를 생성하고 스프링 컨테이너에 등록 return 반환
+//        }
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
