8405ca588e386fbde9b3bcc5de9ed96dac384464
k-minsik <smin58810@gmail.com>
2023-11-07 18:45:32 +0900

    feat: Initialize MVC basic project

---
+++ servlet/src/test/java/kminsik/servlet/ServletApplicationTests.java
@@ -0,0 +1,13 @@
+package kminsik.servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
