2bfb2d8998672d53a8e27a3251f0c78472b7773b
k-minsik <smin58810@gmail.com>
2023-11-17 12:30:57 +0900

    docs: Update README.md

--- item-service/README.md
+++ item-service/README.md
@@ -1,5 +1,27 @@
+## MVC 웹 페이지 만들기
+#### 상품 관리 웹
+
+> 인프런 김영한 강사님 강의수강 목적으로 만들어졌습니다.  
+> https://www.inflearn.com/course/%EC%8A%A4%ED%94%84%EB%A7%81-mvc-1/dashboard
+---
+
+<p align='center'><img src="images/1.png" width="50%" align-img></p>
+<p align='center'><img src="images/2.png" width="50%" align-img></p>
+<p align='center'><img src="images/3.png" width="50%" align-img></p>
+<p align='center'><img src="images/4.png" width="50%" align-img></p>
+<p align='center'><img src="images/5.png" width="50%" align-img></p>
+
+---
+
 1. 요구사항 분석
    1. 상품 도메인 모델
    2. 상품명
 
 2. 상품목록 - 타임리프
+
+3. 상품 상세
+4. 상품 등록 폼
+5. 상품 등록 처리
+6. 상품 수정
+7. PRG
+8. RedirectAttributes
---
+++ item-service/src/main/java/kminsik/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,40 @@
+package kminsik.itemservice.web.basic;
+
+import jakarta.annotation.PostConstruct;
+import kminsik.itemservice.domain.item.Item;
+import kminsik.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+//    @Autowired
+//    public BasicItemController(ItemRepository itemRepository) {
+//        this.itemRepository = itemRepository;
+//    }
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 1000, 10));
+        itemRepository.save(new Item("itemB", 2000, 20));
+    }
+}
